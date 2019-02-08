cbaef38e58800f61dcac710c7fa57d94c35d7dd8
Chris Brown <nudgechris@gmail.com>
2019-02-08 00:02:22 -0600

    first commit

---
+++ scratch.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"outrun", "upstage", "bewitch", "hold hands with", "dance with", "sing to", "ask out", "disturb", "imagine", "in a line up, need to identify", "become dear friends with", "become mortal enemies with", "become more beautiful than", "become", "win a race against", "win an argument with", "get into an agreement with", "win an arm wrestling match to", "lose an arm wrestling match to", "make spaghetti with", "eat ice cream with", "light a candle with", "apply suntan lotion to", "cook with", "cook for", "cook", "forget about", "remember"
+
+"the candlestick maker", "the baker", "the blacksmith", "a bartender", "the busboy at chilis", "that waiter who winked at you one time", "the universe", "god", "the devil", "everyone you know", "everything you touch", "angels", "Leonardo d caprio", "the sinking titanic", "your favorite pet", "your moms casserole", "Picasso", "a bad remake of Die Hard starring Brad Pitt instead of Bruce Willace", "meat gone bad"
