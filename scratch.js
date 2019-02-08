fd70b31b1d4e257a97f41b55ad7b57ec39b51956
Chris Brown <nudgechris@gmail.com>
2019-02-08 10:06:58 -0600

    changed font

--- scratch.js
+++ scratch.js
@@ -12,7 +12,6 @@
 
 
 
+"sign a prenuptial agreement with", "become the beneficiary in the will of", "have a heart-to-heart talk with", "attend rehab with", "go to an AA meeting with", "be the guest on Fresh Air with", "meet an important deadline with", "cheat on an exam", "pee in a cup for", "poop on", "pee on", "take a shower with", "get pooped on by", "get peed on by", "go out to movie and a dinner with", "make a mistake by eating"
 
-"outrun", "upstage", "bewitch", "hold hands with", "dance with", "sing to", "ask out", "disturb", "imagine", "in a line up, need to identify", "become dear friends with", "become mortal enemies with", "become more beautiful than", "become", "win a race against", "win an argument with", "get into an agreement with", "win an arm wrestling match to", "lose an arm wrestling match to", "make spaghetti with", "eat ice cream with", "light a candle with", "apply suntan lotion to", "cook with", "cook for", "cook", "forget about", "remember"
-
-"the candlestick maker", "the baker", "the blacksmith", "a bartender", "the busboy at chilis", "that waiter who winked at you one time", "the universe", "god", "the devil", "everyone you know", "everything you touch", "angels", "Leonardo d caprio", "the sinking titanic", "your favorite pet", "your moms casserole", "Picasso", "a bad remake of Die Hard starring Brad Pitt instead of Bruce Willace", "meat gone bad"
+"a basket full of spiders", "a shopping cart full of chef boy Ardee", "the karate kid", "your soulmate", "that guy from Pizza Hut with acne", "the whole football team", "your high school crush", "your favorite shoes", "Beyoncé’s nipples",
